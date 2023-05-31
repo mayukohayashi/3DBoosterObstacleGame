c895ba68cfdfd1f0428cd22ddd8d27fceb6dabc2
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2023-05-31 17:29:50 +0900

    add: particles

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -11,6 +11,8 @@ public class CollisionHandler : MonoBehaviour
 
     AudioSource audioSource;
 
+    bool isTransitioning = false;
+
     void Start()
     {
         audioSource =GetComponent<AudioSource>();
@@ -18,6 +20,8 @@ public class CollisionHandler : MonoBehaviour
 
     void OnCollisionEnter(Collision other)
     {
+        if (isTransitioning){ return; }
+
         switch (other.gameObject.tag)
         {
             case "Friendly":
@@ -39,6 +43,9 @@ public class CollisionHandler : MonoBehaviour
 
     void StartCrashSequence()
     {
+        isTransitioning = true;
+
+        audioSource.Stop();
         audioSource.PlayOneShot(exploded);
 
         GetComponent<Movement>().enabled = false;
@@ -47,6 +54,9 @@ public class CollisionHandler : MonoBehaviour
 
     void StartNextSceneSequence()
     {
+        isTransitioning = true;
+
+        audioSource.Stop();
         audioSource.PlayOneShot(success);
 
         GetComponent<Movement>().enabled = false;
